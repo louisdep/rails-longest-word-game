b06dd753e43b3e4a91183e78a3c17f7844f25bba
louis depraetere <louisdepraetere@hotmail.com>
2019-05-20 17:30:38 +0200

    making the longest word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,38 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times { @letters << ('a'..'z').to_a.sample }
+  end
+
+  def score
+    @result = params[:word]
+    binding.pry
+    test(@result, @letters)
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+  def test(guess, grid)
+    if included?(guess, grid)
+      binding.pry
+      if english_word?(guess)
+        @score = guess.chars.length
+      else
+        @scoure = 0
+      end
+    else
+      @score = 0
+    end
+  end
+end
